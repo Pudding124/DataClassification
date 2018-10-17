83befcff2536329cb9d99eb3073008779274ce1a
許銘仁 <surprised128@gmail.com>
2018-10-17 23:22:35 +0800

    fix read problem

--- src/main/java/ntou/soselab/dictionary/algo/Word2vec.java
+++ src/main/java/ntou/soselab/dictionary/algo/Word2vec.java
@@ -10,21 +10,27 @@ import java.io.*;
 public class Word2vec {
     Logger log = LoggerFactory.getLogger(Word2vec.class);
 
+    // windowns 10 connect python
     public void useWord2vecWithPython(String str, String str2) throws IOException, InterruptedException {
         // String pythonPath = "E://word2vec/main.py";
         ProcessBuilder pb = new ProcessBuilder("python", "E:/word2vec/main.py", "weather");
         pb.redirectErrorStream(true);
         Process p = pb.start();
 
-        char[] readBuffer = new char[1000];
         InputStreamReader isr = new InputStreamReader(p.getInputStream());
         BufferedReader br = new BufferedReader(isr);
-
-        while (true) {
-            int n = br.read(readBuffer);
-            if (n <= 0)
-                break;
-            System.out.print(new String(readBuffer, 0, n));
+        String response = br.readLine();
+        String res[] = response.replace(" ","").split(",");
+        for(String str3 : res){
+            String str4 = str3.replaceAll("\\p{P}","");
+            log.info(str4);
         }
+        log.info("response :{}", response);
+//        while (true) {
+//            int n = br.read(readBuffer);
+//            if (n <= 0)
+//                break;
+//            System.out.print(new String(readBuffer, 0, n));
+//        }
     }
 }
