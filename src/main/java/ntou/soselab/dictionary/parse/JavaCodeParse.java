d2afe9d7be032db5761200b1e691135512647359
mingjen <surprised128@gmail.com>
2018-11-13 21:20:04 +0800

    add some JavaCodeParse method

--- src/main/java/ntou/soselab/dictionary/parse/JavaCodeParse.java
+++ src/main/java/ntou/soselab/dictionary/parse/JavaCodeParse.java
@@ -3,9 +3,7 @@ package ntou.soselab.dictionary.parse;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.expr.BinaryExpr;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
@@ -27,7 +25,7 @@ public class JavaCodeParse {
 
     public void getJavaMethodUse() throws IOException {
         // creates an input stream for the file to be parsed
-        FileInputStream in = new FileInputStream("E:\\STS_Test\\Dictionary\\src\\main\\resources\\CartServiceImpl.java");
+        FileInputStream in = new FileInputStream("/home/mingjen/IdeaProjects/DataClassification/src/main/resources/CartServiceImpl.java");
 
         // parse it
         CompilationUnit cu = JavaParser.parse(in);
@@ -39,7 +37,6 @@ public class JavaCodeParse {
 //        for(ImportDeclaration str : cu.getImports()) {
 //            System.out.println("Import :" + str);
 //        }
-
     }
 
     private static class MethodVisitor extends VoidVisitorAdapter<Void> {
@@ -48,7 +45,8 @@ public class JavaCodeParse {
             /* here you can access the attributes of the method.
              this method will be called for all methods in this
              CompilationUnit, including inner class methods */
-            System.out.println(n.getName()+n.getBody().toString());
+            System.out.println(n.getName());
+            System.out.println(n.getBody().toString());
             System.out.println("-----------------------------------------------------");
             super.visit(n, arg);
         }
@@ -62,10 +60,20 @@ public class JavaCodeParse {
          CompilationUnit, including inner class methods */
             System.out.println(n.getFields());
             System.out.println("-----------------------------------------------------");
+
+            // get class variable name and content
             for(FieldDeclaration field : n.getFields()) {
-                String content[] = field.getVariables().toString().split("=");
-                System.out.println(content[1].replaceAll("\\p{P}",""));
+                System.out.println(field.getVariables());
+                for(VariableDeclarator variable : field.getVariables()) {
+                    System.out.println(variable.getName() + " : " + variable.getInitializer().get());
+                }
             }
+
+            for(MethodDeclaration method : n.getMethods()) {
+                System.out.println("Name :" + method.getName());
+                System.out.println("Body :" + method.getBody().get());
+            }
+
             super.visit(n, arg);
         }
     }
